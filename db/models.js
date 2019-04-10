f76ee6ffeb1f51b96841af1ed3d82677317451ef
Preston Chaplin <preston.chaplin@gmail.com>
2019-04-10 00:25:22 -0400

    xfer to dt

--- db/models.js
+++ db/models.js
@@ -1,34 +1,40 @@
-const sequelize = require('sequelize');
-const conn = require('./index');
+const sequelize = require("sequelize");
+const conn = require("./index");
 
-const User = conn.define('user', {
+const User = conn.define("user", {
   name: {
     type: sequelize.STRING,
     allowNull: false
   }
 });
 
-const Product = conn.define('product', {
+const Product = conn.define("product", {
   name: {
     type: sequelize.STRING,
     allowNull: false
   }
 });
 
-Product.belongsTo(User, { as: 'manager' });
-User.hasMany(Product);
+Product.belongsTo(User, { as: "manager" });
+// User.hasMany(Product);
 
 const syncAndSeed = () => {
-  return conn.sync({ force: true }).then(() => {
-    return Promise.all([
-      User.create({ name: 'Moe' }),
-      User.create({ name: 'Larry' }),
-      User.create({ name: 'Curly' }),
-      Product.create({ name: 'Foo', managerId: 3 }),
-      Product.create({ name: 'Bar', managerId: 2 }),
-      Product.create({ name: 'Baz', managerId: 3 })
-    ]);
-  });
+  return conn
+    .sync({ force: true })
+    .then(() => {
+      return Promise.all([
+        User.create({ name: "Moe" }),
+        User.create({ name: "Larry" }),
+        User.create({ name: "Curly" })
+      ]);
+    })
+    .then(() => {
+      return Promise.All([
+        Product.create({ name: "Foo", managerId: 3 }),
+        Product.create({ name: "Bar", managerId: 2 }),
+        Product.create({ name: "Baz", managerId: 3 })
+      ]);
+    });
 };
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,38 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const { syncAndSeed, User, Product } = require('./db/models');
+const path = require("path");
+const { syncAndSeed, User, Product } = require("./db/models");
 
 app.use(express.json());
 
-app.get('/app.js', (req, res, next) =>
-  res.sendFile(path.join(__dirname, 'dist', 'main.js')));
+app.get("/app.js", (req, res, next) =>
+  res.sendFile(path.join(__dirname, "dist", "main.js"))
+);
 
-app.get('/', (req, res, next) =>
-  res.sendFile(path.join(__dirname, 'index.html')));
+app.get("/", (req, res, next) =>
+  res.sendFile(path.join(__dirname, "index.html"))
+);
 
-app.get('/api/managers', (req, res, next) => {
+app.get("/api/managers", (req, res, next) => {
   User.findAll().then(users => res.send(users));
 });
 
-app.get('/api/products', (req, res, next) => {
+app.get("/api/products", (req, res, next) => {
   Product.findAll()
     .then(products => res.send(products))
     .catch(next);
 });
 
-app.put('/api/products/:id', (req, res, next) => {
+app.put("/api/products/:id", (req, res, next) => {
   Product.findByPk(req.params.id)
     .then(product => product.update(req.body))
     .then(product => res.send(product))
     .catch(next);
 });
 
-// app.get('/api/products/:id', (req, res, next) => {
-//   Product.findByPk(req.params.id)
-//     .then(product => res.send(product))
-//     .catch(next);
-// });
+app.get("/api/products/:id", (req, res, next) => {
+  Product.findByPk(req.params.id)
+    .then(product => res.send(product))
+    .catch(next);
+});
 
 const port = process.env.PORT || 3000;
 
